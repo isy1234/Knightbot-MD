d975b21144992109bc34e35463c2ebb28050aa67
isy1234 <theophilusdadzie703@gmail.com>
2025-06-06 18:36:07 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'ℝ𝕙𝕚𝕔𝕙𝕞𝕒𝕟 ℙ𝕠𝕚𝕤𝕪', // Your name
+  ownerNumber: '233593476177', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.2",
 };
