2108dc7e207cdabf43c9dc9abde5dccd7af6aee1
isy1234 <theophilusdadzie703@gmail.com>
2025-06-07 07:00:33 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'ℝ𝕙𝕚𝕔𝕙𝕞𝕒𝕟 ℙ𝕠𝕚𝕤𝕪', // Your name
-  ownerNumber: '233593476177', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '233533466459', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
